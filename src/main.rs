457a0ec63607451792f8fe20c4d10b8316ed13fd
pseres <peet.seres@gmail.com>
2021-05-11 12:46:03 +0200

    added glium window

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,52 @@
+use glium::{glutin, Surface};
+use std::time::{Instant, Duration};
+
+const WIDTH: u32 = 900;
+const HEIGHT: u32 = 700;
+const TIME_PER_FRAME: u64 = 16_666_667;
+
+
 fn main() {
-    println!("Initializing SDL window context.");
-    let sdl = sdl2::init().unwrap();
-    let video_subsystem = sdl.video().unwrap();
+    println!("Initializing GLIUM window context.");
 
-    let window = video_subsystem
-        .window("Open GL Window WIP", 900, 700)
-        .opengl()                                           // Add openGL Flag
-        .resizable()
-        .build()
-        .unwrap();
+    // Event handling
+    let mut event_loop = glutin::event_loop::EventLoop::new();
 
-    let mut event_pump = sdl.event_pump().unwrap();
+    // Application parameter specifier:
+    let window_builder = glutin::window::WindowBuilder::new()
+        .with_inner_size(glutin::dpi::LogicalSize::new(WIDTH, HEIGHT))
+        .with_title("Open GL Hello World!");
 
+    // Open GL context specifier:
+    let context_builder = glutin::ContextBuilder::new();
 
-    println!("Initializing OpenGL context.");
-    let gl_context = window.gl_create_context().unwrap();
-    let gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
-
-    unsafe {
-        gl::ClearColor(0.133, 0.2353, 0.3019, 1.0);
-    }
+    // Display object: OpenGL window
+    let display = glium::Display::new(window_builder, context_builder, &event_loop).unwrap();
 
     // Infinite loop to keep the program running
-    'main_loop: loop {
+    event_loop.run(move |ev, _, control_flow| {
+
+        // Render stuff:
+        let mut target = display.draw();
+        target.clear_color(0.1, 0.19, 0.43, 1.0);
+        target.finish().unwrap();
+
+        // Limit flow to 60 FPS
+        let next_frame_time = Instant::now() + Duration::from_nanos(TIME_PER_FRAME);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
 
         // Handle events
-        for event in event_pump.poll_iter() {
-            match event {
-                sdl2::event::Event::Quit {..} => {
-                    println!("Quit Event. Closing main loop.");
-                    break 'main_loop;
-                },
-                _ => {}
-            }
+        match ev {
+            glutin::event::Event::WindowEvent {event, ..} => {
+                match event {
+                    glutin::event::WindowEvent::CloseRequested => {
+                        *control_flow = glutin::event_loop::ControlFlow::Exit;
+                        return;
+                    },
+                    _ => return
+                }
+            },
+            _ => {},
         }
-
-        // Render window contents
-        unsafe {
-            gl::Clear(gl::COLOR_BUFFER_BIT);
-        }
-
-        window.gl_swap_window();
-    }
-
+    });
 }
