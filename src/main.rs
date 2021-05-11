dc9e1beaed7ebfed9e0a5a9248fffa78b8a43281
pseres <peet.seres@gmail.com>
2021-05-11 11:54:27 +0200

    added open gl context and fill color

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 fn main() {
-    println!("Initializing SDL context.");
+    println!("Initializing SDL window context.");
     let sdl = sdl2::init().unwrap();
     let video_subsystem = sdl.video().unwrap();
+
     let window = video_subsystem
         .window("Open GL Window WIP", 900, 700)
+        .opengl()                                           // Add openGL Flag
         .resizable()
         .build()
         .unwrap();
+
     let mut event_pump = sdl.event_pump().unwrap();
+
+
+    println!("Initializing OpenGL context.");
+    let gl_context = window.gl_create_context().unwrap();
+    let gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+
+    unsafe {
+        gl::ClearColor(0.133, 0.2353, 0.3019, 1.0);
+    }
 
     // Infinite loop to keep the program running
     'main_loop: loop {
@@ -15,12 +27,20 @@ fn main() {
         // Handle events
         for event in event_pump.poll_iter() {
             match event {
-                sdl2::event::Event::Quit {..} => break 'main_loop,
+                sdl2::event::Event::Quit {..} => {
+                    println!("Quit Event. Closing main loop.");
+                    break 'main_loop;
+                },
                 _ => {}
             }
         }
 
         // Render window contents
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
 
+        window.gl_swap_window();
     }
+
 }
