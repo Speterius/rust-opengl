ec43623ad8ef354804321100c5dcd18919980544
pseres <peet.seres@gmail.com>
2021-05-11 13:21:54 +0200

    Added vertex animation

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,16 @@ fn define_shaders() -> (String, String) {
     let vertex_shader: String = String::from(
         r#"
         #version 140
+
         in vec2 position;
+
+        uniform float dx;
+
         void main() {
-            gl_Position = vec4(position, 0.0, 1.0);
+
+            vec2 pos = position;
+            gl_Position = vec4(pos.x+dx, pos.y, 0.0, 1.0);
+
         }
     "#,
     );
@@ -42,7 +49,7 @@ fn main() {
     println!("Initializing GLIUM window context.");
 
     // Event handling
-    let mut event_loop = glutin::event_loop::EventLoop::new();
+    let event_loop = glutin::event_loop::EventLoop::new();
 
     // Application parameter specifier:
     let window_builder = glutin::window::WindowBuilder::new()
@@ -55,11 +62,32 @@ fn main() {
     // Display object: OpenGL window
     let display = glium::Display::new(window_builder, context_builder, &event_loop).unwrap();
 
+    // Define triangle
+    let vertex1 = Vertex {
+        position: [-0.5, -0.5],
+    };
+    let vertex2 = Vertex {
+        position: [0.0, 0.5],
+    };
+    let vertex3 = Vertex {
+        position: [0.5, -0.25],
+    };
+    let shape = vec![vertex1, vertex2, vertex3];
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    // Some animation variables
+    let start = Instant::now();
+
     // Infinite loop to keep the program running
     event_loop.run(move |ev, _, control_flow| {
         // Limit flow to 60 FPS
         let next_frame_time = Instant::now() + Duration::from_nanos(TIME_PER_FRAME);
         *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+
+        // Change animation variable:
+        let t = Instant::now().duration_since(start).as_secs_f32();
+        let x = 0.5 * (0.5 * t * 2.0 * std::f32::consts::PI).sin();
 
         // Handle events
         match ev {
@@ -79,20 +107,6 @@ fn main() {
         // Clear screen to blue:
         target.clear_color(0.1, 0.19, 0.43, 1.0);
 
-        // Define triangle
-        let vertex1 = Vertex {
-            position: [-0.5, -0.5],
-        };
-        let vertex2 = Vertex {
-            position: [0.0, 0.5],
-        };
-        let vertex3 = Vertex {
-            position: [0.5, -0.25],
-        };
-        let shape = vec![vertex1, vertex2, vertex3];
-        let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-        let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
-
         // Shaders:
         let (vertex_shader, fragment_shader) = define_shaders();
         let program =
@@ -104,7 +118,7 @@ fn main() {
                 &vertex_buffer,
                 &indices,
                 &program,
-                &glium::uniforms::EmptyUniforms,
+                &glium::uniform! {dx: x},
                 &Default::default(),
             )
             .unwrap();
