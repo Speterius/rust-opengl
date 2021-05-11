2559cbf17c1e1bc382fd512e9891317b5a70c8f4
pseres <peet.seres@gmail.com>
2021-05-11 21:38:25 +0200

    added Blinn-phong shading

--- src/shaders/fragment_shader.glsl
+++ src/shaders/fragment_shader.glsl
@@ -1,12 +1,27 @@
 #version 140
 
 in vec3 v_normal;
+in vec3 v_position;
+
 out vec4 color;
+
 uniform vec3 u_light;
 
+const vec3 ambient_color = vec3(0.2, 0.0, 0.0);
+const vec3 diffuse_color = vec3(0.6, 0.0, 0.0);
+const vec3 specular_color = vec3(1.0, 1.0, 1.0);
+
 void main() {
-    float brightness = dot(normalize(v_normal), normalize(u_light));
-    vec3 dark_color = vec3(0.6, 0.0, 0.0);
-    vec3 regular_color = vec3(1.0, 0.0, 0.0);
-    color = vec4(mix(dark_color, regular_color, brightness), 1.0);
+
+    vec3 N = normalize(v_normal);
+    vec3 L = normalize(u_light);
+    vec3 C = normalize(-v_position);
+
+    float diffuse = max(dot(N, L), 0.0);
+
+    vec3 half_direction = normalize(L + C);
+
+    float specular = pow(max(dot(half_direction, N), 0.0), 16.0);
+
+    color = vec4(ambient_color + diffuse * diffuse_color + specular * specular_color, 1.0);
 }
--- src/shaders/vertex_shader.glsl
+++ src/shaders/vertex_shader.glsl
@@ -4,6 +4,7 @@ in vec3 position;
 in vec3 normal;
 
 out vec3 v_normal;
+out vec3 v_position;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -13,4 +14,5 @@ void main() {
     mat4 modelview = view * model;
     v_normal = transpose(inverse(mat3(modelview))) * normal;
     gl_Position = perspective * modelview * vec4(position, 1.0);
+    v_position = gl_Position.xyz / gl_Position.w;
 }
